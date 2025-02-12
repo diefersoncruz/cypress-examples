f3a4d43a7a75751ab5b6e0b22a3ece14e5f7f3fe
Dieferson Cruz <dieferson.cruz@outlook.com>
2025-02-12 15:45:39 -0300

    removido dependencia não utilizada.

--- cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
-
-// Adicione as etapas do seu cenário aqui
-
-Given("I visit the homepage", () => {});
-
-When("I click on the login button", () => {});
-
-Then("I should see the login page", () => {});
